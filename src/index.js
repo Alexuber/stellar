bd55914d737bb6c0180542a38d587b2548e4daa3
Alexuber <alexuberbuber@gmail.com>
2023-07-15 23:09:33 +0300

    added sect geography

--- src/index.js
+++ src/index.js
@@ -1,24 +1,73 @@
-const slider = document.querySelector('.slider');
-const cards = document.querySelectorAll('.card');
-const prevButton = document.querySelector('#prevButton');
-const nextButton = document.querySelector('#nextButton');
+const swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  loop: true,
+  width: 320,
+  spaceBetween: 8,
+  navigation: {
+    enabled: true,
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
-let currentIndex = 0;
-cards[currentIndex].classList.add('active');
+// Form order
+// Получаем все шаги формы
+const steps = document.querySelectorAll('.step');
+const paginationItems = document.querySelectorAll('.pagination-item');
 
-function showCard(index) {
-  cards[currentIndex].classList.remove('active');
-  currentIndex = (index + cards.length) % cards.length;
-  cards[currentIndex].classList.add('active');
+// Переключение на следующий шаг
+function nextStep() {
+  // Находим текущий активный шаг
+  const currentStep = document.querySelector('.step.active');
+  const currentPaginationItem = document.querySelector(
+    '.pagination-item.active'
+  );
+
+  // Проверяем, есть ли следующий шаг
+  if (currentStep.nextElementSibling) {
+    // Скрываем текущий шаг и показываем следующий шаг
+    currentStep.classList.remove('active');
+    currentStep.nextElementSibling.classList.add('active');
+  }
+
+  if (currentPaginationItem.nextElementSibling) {
+    // Скрываем текущий шаг и показываем следующий шаг
+    currentPaginationItem.classList.remove('active');
+    currentPaginationItem.nextElementSibling.classList.add('active');
+  }
 }
 
-function prevCard() {
-  showCard(currentIndex - 1);
+// Переключение на предыдущий шаг
+function prevStep() {
+  // Находим текущий активный шаг
+  const currentStep = document.querySelector('.step.active');
+
+  // Проверяем, есть ли предыдущий шаг
+  if (currentStep.previousElementSibling) {
+    // Скрываем текущий шаг и показываем предыдущий шаг
+    currentStep.classList.remove('active');
+    currentStep.previousElementSibling.classList.add('active');
+  }
 }
 
-function nextCard() {
-  showCard(currentIndex + 1);
-}
+// Обработчик события для кнопок "Далее"
+const nextButtons = document.querySelectorAll('.next-button');
+nextButtons.forEach(button => {
+  button.addEventListener('click', nextStep);
+});
 
-prevButton.addEventListener('click', prevCard);
-nextButton.addEventListener('click', nextCard);
+const checkboxButtons = document.querySelectorAll('.checkbox-button');
+
+checkboxButtons.forEach(button => {
+  const checkbox = button.querySelector('input[type="checkbox"]');
+
+  checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      button.classList.add('selected');
+    } else {
+      button.classList.remove('selected');
+    }
+  });
+});
